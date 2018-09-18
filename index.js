24672c603a7d6ea2fa99494a37a5629e47524dc2
wangyan6783 <wangyan6783@gmail.com>
2018-09-18 12:09:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, obj, arg){
+  return fn.call(obj, arg);
+}
+
+function setThisWithApply(fn, obj, args){
+  return fn.apply(obj, args);
+}
+
+function returnNewFunctionOf(fn, obj){
+  const newFn = fn.bind(obj);
+  return newFn;
+}
