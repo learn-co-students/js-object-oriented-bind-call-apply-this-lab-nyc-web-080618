1d6b3bfd571d20cf9f06af90e2bd78877462a87f
mccoleman75225 <michaelcoleman19@gmail.com>
2018-09-18 13:00:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+
+const justInvoke = function_name => function_name()
+
+const setThisWithCall = function (set_function, entered_value, argument) {
+  return set_function.call(entered_value, argument);
+}
+
+const setThisWithApply = function (set_function, entered_value, argument) {
+  return set_function.apply(entered_value, argument);
+}
+
+
+const returnNewFunctionOf = function(set_function,argument){
+  const new_function = set_function.bind(argument);
+  return new_function;
+}
